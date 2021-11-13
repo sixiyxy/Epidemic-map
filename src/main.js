b704716af7378a5018dd6621b244dd4af35df122
sixiyxy <771702500@qq.com>
2021-11-13 15:25:39 +0800

    build: 增加代码规范相关配置以及库

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import "./assets/css/common.css"
+import api from "./api/index"
+
+Vue.prototype.$api = api;
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
