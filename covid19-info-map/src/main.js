d06a38dd44b5fca234539328625600e1422df82f
sixiyxy <771702500@qq.com>
2021-11-13 10:48:26 +0800

    covid19-info

--- covid19-info-map/src/main.js
+++ covid19-info-map/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import "./assets/css/common.css"
+import api from "./api/index"
 
+Vue.prototype.$api = api;
 Vue.config.productionTip = false
 
 new Vue({
--- covid19-info-map/src/views/Home.vue
+++ covid19-info-map/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Info :infoData="infoData"/>
   </div>
 </template>
 
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "../components/Header"
+import Info from "../components/Info"
 
 export default {
-  name: 'Home',
+  name: "Home",
+  data(){
+    return{
+      infoData:{}
+    }
+  },
+  created(){
+    this.$api.getDataInfo({
+      key:"0b2292c01bf100466bc2336564f17674"
+    }).then(res => {
+      console.log(res)
+      this.infoData = {
+        note1:res.newslist[0].desc.note1,
+        note2:res.newslist[0].desc.note2,
+        note3:res.newslist[0].desc.note3,
+        remark1:res.newslist[0].desc.remark1,
+        remark2:res.newslist[0].desc.remark2,
+        remark3:res.newslist[0].desc.remark3
+      }
+    })
+  },
   components: {
-    HelloWorld
+    Header, 
+    Info
   }
 }
 </script>
+
