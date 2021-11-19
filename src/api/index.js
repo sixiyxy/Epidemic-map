adc426582239a0482d5b819dc989072d5f8685b7
sixiyxy <771702500@qq.com>
2021-11-19 14:34:47 +0800

    display

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import { get } from "../utils/request";
 
 const api = {
   getDataInfo(params) {
-    return get("http://api.tianapi.com/ncov/index", params);
+    return get("https://api.tianapi.com/ncov/index", params);
   },
   getCaseNum(params){
     return get("https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5")
